5af36f6e0d09a0213c33984a631b8a61250f6a6d
ganheather02 <ganheather02@gmail.com>
2020-01-25 21:03:57 -0800

    Initial Commit

---
+++ rtippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rtippy
+//
+//  Created by Heather Gan on 1/25/20.
+//  Copyright © 2020 tt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
